8f944387f31dd7bbf373d9aaf5bb185c233a879e
K Asante <73370911+kasante1@users.noreply.github.com>
2023-04-06 11:39:04 +0000

    fetchcli  arguments

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	//"log"
+	"errors"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Goblin!\n Usage: -bin|-lib <project_name>")
+
+	if len(os.Args) == 1 {
+		fmt.Println("Enter a project name.")
+		return
+	}
+
+	// get project name
+	arguments := os.Args
+
+	project_name := arguments[1]
+
+	// checked if the argument supplied is a directory
+
+	fileOrDirectory, err := IsArgumentDirectory(project_name)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if fileOrDirectory {
+		if _, err := os.Stat(project_name); errors.Is(err, os.ErrNotExist) {
+			err := os.Mkdir(project_name, os.ModePerm)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+
+	} else {
+		// get current directory or where to save
+		// project
+
+		path, err := os.Getwd()
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// fmt.Println(path)
+
+		// create project directory and project name
+		project_directory := filepath.Join(path, project_name)
+
+		fmt.Println(project_directory)
+
+		// if err := os.Mkdir("a", os.ModePerm); err != nil {
+		//     log.Fatal(err)
+		// }
+
+		// check if the directory does not exits
+		if _, err := os.Stat(project_directory); errors.Is(err, os.ErrNotExist) {
+			err := os.Mkdir(project_directory, os.ModePerm)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+
+}
+
+// is the provided argument a string
+// or a directory
+
+func IsArgumentDirectory(argument string) (bool, error) {
+
+	if strings.Contains(argument, "\"") || strings.Contains(argument, "/") {
+		return true, nil
+	}
+	return false, errors.New("a string not a directory")
+
+}
