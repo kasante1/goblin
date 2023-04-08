021da6538dc4ec62993d43595d98ed33ce60887f
K Asante <73370911+kasante1@users.noreply.github.com>
2023-04-08 14:35:46 +0000

    multiple checks for cli argument

--- main.go
+++ main.go
@@ -22,12 +22,8 @@ func main() {
 
 	project_name := arguments[1]
 
-	// checked if the argument supplied is a directory
-	fileOrDirectory, err := IsArgumentDirectory(project_name)
-
-	if err != nil {
-		fmt.Println(err)
-	}
+	// checked if the cli_argument supplied is a directory
+	fileOrDirectory := IsArgumentDirectory(project_name)
 
 	if fileOrDirectory {
 		dir_status := AlreadyExist(project_name)
@@ -37,6 +33,7 @@ func main() {
 		}
 
 	} else {
+
 		// get current directory or where to save
 		// project
 
@@ -45,8 +42,6 @@ func main() {
 		if err != nil {
 			fmt.Println(err)
 		}
-
-		// fmt.Println(path)
 
 		// create project directory and project name
 		project_directory := filepath.Join(path, project_name)
@@ -62,37 +57,76 @@ func main() {
 
 }
 
-// is the provided argument a string
+// is the provided cli_argument a string
 // or a directory
 
-func IsArgumentDirectory(argument string) (bool, error) {
+func IsArgumentDirectory(cli_argument string) bool {
 
-	if strings.Contains(argument, "\"") || strings.Contains(argument, "/") {
-		return true, nil
+	// split cli_argument and test if its a direcoty minus
+	// the cli_argument is a directory
+
+	if strings.Contains(cli_argument, "/") {
+
+		dir_argument, _ := SplitArgument(cli_argument)
+		// check if the dir_argument is a directory
+		if _, err := os.Stat(dir_argument); errors.Is(err, os.ErrExist) {
+			return true
+		}
+		//check if the cli_argument is an existing directory
+	} else if _, err := os.Stat(cli_argument); errors.Is(err, os.ErrExist) {
+		return true
 	}
-	return false, errors.New("a string not a directory")
-
+	
+	return false
 }
 
-// is the argument provided an existing directory
+// split cli_argument if it contains a slash
+func SplitArgument(cli_argument string) (string, string) {
+
+	// split cli_argument by the slash delimiter
+	split_argument := strings.Split(cli_argument, "/")
+
+	//get string slice length
+	split_argument_length := len(split_argument) - 1
+
+	// get last cli_argument string
+	argument_remainder := strings.Join(split_argument[split_argument_length:], "")
+
+	return split_argument[split_argument_length], argument_remainder
+}
+
+// is the cli_argument provided an existing directory
 // project name
-
 func AlreadyExist(cli_argument string) error {
-	if _, err := os.Stat(cli_argument); errors.Is(err, os.ErrNotExist) {
-		err := os.Mkdir(cli_argument, os.ModePerm)
-		if err != nil {
-			fmt.Println(err)
-		}
 
-		fmt.Println("project directory created here :", cli_argument)
+	// is the cli_argument provided an existing directory
+	if _, err := os.Stat(cli_argument); errors.Is(err, os.ErrNotExist) {
+
+		// create directory/folder
+		create_project := CreateProjectDirectory(cli_argument)
+
+		// stop if directory creation fails
+		if create_project != nil {
+			return create_project
+		}
 
 		return nil
 
 	} else {
-		//fmt.Println(cli_argument, " already exits!")
 
 		error_message := cli_argument + " already exits!"
 
 		return errors.New(error_message)
 	}
+}
+
+// create project directory/folders
+func CreateProjectDirectory(cli_argument string) error {
+	err := os.Mkdir(cli_argument, os.ModePerm)
+	if err != nil {
+		return errors.New("project creation failed. check directory permission")
+	}
+
+	fmt.Println("project directory created here :", cli_argument)
+	return nil
 }
