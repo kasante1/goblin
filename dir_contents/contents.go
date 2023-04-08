c4c59e27bc637bbf4e80c62d13c5cd0c130afc71
K Asante <73370911+kasante1@users.noreply.github.com>
2023-04-08 20:29:29 +0000

    implement directory contents

---
+++ dir_contents/contents.go
@@ -0,0 +1,27 @@
+package dir_contents 
+
+
+// write files into the directories created
+
+// create
+
+// go mod files
+
+// test files
+
+// sample hello world file
+
+// package main file
+
+// folder package file
+
+
+func Create_mod_file(){}
+
+func Create_test_files(){}
+
+func Create_main_project_file()
+
+// use to illustrate how packages
+// work in go lang
+func Create_folder_package(){}
--- main.go
+++ main.go
@@ -2,131 +2,42 @@ package main
 
 import (
 	"fmt"
+	"goblin/cli_args"
 	"os"
-	//"log"
-	"errors"
-	"path/filepath"
-	"strings"
 )
 
 func main() {
-	fmt.Println("Goblin!\n Usage: -bin|-lib <project_name>")
+	fmt.Println("Goblin!\n Usage: -bin|-lib <cli_project_argument>")
 
 	if len(os.Args) == 1 {
 		fmt.Println("Enter a project name.")
 		return
 	}
 
-	// get project name
-	arguments := os.Args
+	// get fetch cli_arguments
+	cli_arguments := os.Args
 
-	project_name := arguments[1]
+	// get cli project argument
+	cli_project_argument := cli_arguments[1]
 
 	// checked if the cli_argument supplied is a directory
-	fileOrDirectory := IsArgumentDirectory(project_name)
+	var fileOrDirectory bool = cli_args.IsArgumentDirectory(cli_project_argument)
+
+	// if cli is a directory notify user or
+	// create project in the existing directory
 
 	if fileOrDirectory {
-		dir_status := AlreadyExist(project_name)
+		var dir_status error = cli_args.AlreadyExist(cli_project_argument)
 
 		if dir_status != nil {
 			fmt.Println(dir_status)
 		}
 
 	} else {
-
-		// get current directory or where to save
-		// project
-
-		path, err := os.Getwd()
-
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		// create project directory and project name
-		project_directory := filepath.Join(path, project_name)
-
-		// check if the directory does not exits
-		file_status := AlreadyExist(project_directory)
-
-		if file_status != nil {
-			fmt.Println(file_status)
-		}
+		// if cli_project_argument is not a directory
+		// create new directory in CWD
+		cli_args.NewProjectDirectry(cli_project_argument)
 
 	}
 
-}
-
-// is the provided cli_argument a string
-// or a directory
-
-func IsArgumentDirectory(cli_argument string) bool {
-
-	// split cli_argument and test if its a direcoty minus
-	// the cli_argument is a directory
-
-	if strings.Contains(cli_argument, "/") {
-
-		dir_argument, _ := SplitArgument(cli_argument)
-		// check if the dir_argument is a directory
-		if _, err := os.Stat(dir_argument); errors.Is(err, os.ErrExist) {
-			return true
-		}
-		//check if the cli_argument is an existing directory
-	} else if _, err := os.Stat(cli_argument); errors.Is(err, os.ErrExist) {
-		return true
-	}
-	
-	return false
-}
-
-// split cli_argument if it contains a slash
-func SplitArgument(cli_argument string) (string, string) {
-
-	// split cli_argument by the slash delimiter
-	split_argument := strings.Split(cli_argument, "/")
-
-	//get string slice length
-	split_argument_length := len(split_argument) - 1
-
-	// get last cli_argument string
-	argument_remainder := strings.Join(split_argument[split_argument_length:], "")
-
-	return split_argument[split_argument_length], argument_remainder
-}
-
-// is the cli_argument provided an existing directory
-// project name
-func AlreadyExist(cli_argument string) error {
-
-	// is the cli_argument provided an existing directory
-	if _, err := os.Stat(cli_argument); errors.Is(err, os.ErrNotExist) {
-
-		// create directory/folder
-		create_project := CreateProjectDirectory(cli_argument)
-
-		// stop if directory creation fails
-		if create_project != nil {
-			return create_project
-		}
-
-		return nil
-
-	} else {
-
-		error_message := cli_argument + " already exits!"
-
-		return errors.New(error_message)
-	}
-}
-
-// create project directory/folders
-func CreateProjectDirectory(cli_argument string) error {
-	err := os.Mkdir(cli_argument, os.ModePerm)
-	if err != nil {
-		return errors.New("project creation failed. check directory permission")
-	}
-
-	fmt.Println("project directory created here :", cli_argument)
-	return nil
 }
