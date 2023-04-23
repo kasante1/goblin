bb265dc1e55e00351b752c34e7552943b2131d0b
K Asante <dsasante1@gmail.com>
2023-04-23 18:22:29 +0000

    set sub directory, files creators

--- dir_contents/contents.go
+++ dir_contents/contents.go
@@ -1,5 +1,11 @@
-package dir_contents 
+package dir_contents
 
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+)
 
 // write files into the directories created
 
@@ -15,13 +21,36 @@ package dir_contents
 
 // folder package file
 
+func Create_project_files(SubDirectories, fileName string) {
+	_, err := os.Stat(SubDirectories)
 
-func Create_mod_file(){}
+	if errors.Is(err, os.ErrNotExist) {
+		fmt.Println("file does not exit")
+	} else {
+		fmt.Println("file exits")
+	}
+}
 
-func Create_test_files(){}
+func Write_project_files(fileName string) {
+	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	if _, err := file.WriteString("some project stuff"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Create_mod_file() {}
+
+func Create_test_files() {}
 
 func Create_main_project_file()
 
 // use to illustrate how packages
 // work in go lang
-func Create_folder_package(){}
+func Create_folder_package() {}
